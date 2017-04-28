95efa01bc8304daf35dd4852cc03b41a506c49f3
Ryan <ninja@snoring.ninja>
2017-04-27 23:27:55 -0400

    Owner id from settings file.

--- commands/logout.py
+++ commands/logout.py
@@ -1,16 +1,18 @@
 import discord
 from discord.ext import commands
 import os, sys
+from resources.resourcepath import *
+from resources.config import BOTSETTINGS, load_config, ConfigLoader
 
 class logout():
 	def __init__(self, bot):
 		self.bot = bot
-		self.owner_id = 104771621687980032
+		self.owner_id = ConfigLoader().load_config_setting(BOTSETTINGS, 'owner_id')
 
 	@commands.command(pass_context=True, no_pm=True)
 	async def logout(self, ctx, member: discord.Member = None):
 		userID = ctx.message.author.id
-		print(userID)
+
 		if int(userID) == self.owner_id:
 			await self.bot.say("Shutting down, bye!")
 			await self.bot.logout()
