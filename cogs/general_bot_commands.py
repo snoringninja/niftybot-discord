f0f9411e8e8fe86d21f84d0cc58cfd2bce69aa43
xNifty <ninja@snoring.ninja>
2017-10-09 12:51:08 -0400

    More PEP8 changes, correct function names that were changed

--- cogs/general_bot_commands.py
+++ cogs/general_bot_commands.py
@@ -1,10 +1,16 @@
-# Imports for ConfigUpdater
-import discord
-import logging
-from discord.ext import commands
-import os
-import configparser
+"""
+general_bot_commands.py
+@author Ryan 'iBeNifty' Malacina
+@site https://snoring.ninja
+
+Class to handle general bot commands that have no
+place in other classes.
+"""
+
 import traceback
+
+import discord
+from discord.ext import commands
 
 from resources.database import DatabaseHandler
 from resources.config import ConfigLoader
@@ -12,52 +18,78 @@ from resources.error import ErrorLogging
 from resources.general_resources import BotResources
 
 class BotCommands:
-	def __init__(self, bot):
-		self.bot = bot
-		self.prefix = ConfigLoader().load_config_setting('BotSettings', 'command_prefix')
+    """
+    Class to control multiple different functions that
+    have no place in other classes.
+    """
+    def __init__(self, bot):
+        self.bot = bot
+        self.prefix = ConfigLoader().load_config_setting('BotSettings', 'command_prefix')
 
-	@commands.command(pass_context=True, no_pm=False, name='accept')
-	@commands.cooldown(rate=1, per=1, type=commands.BucketType.user)
-	async def add_accepted_user(self, ctx, member: discord.Member = None):
-		member = ctx.message.author
-		memberID = ctx.message.author.id
+    @commands.command(pass_context=True, no_pm=False, name='accept')
+    @commands.cooldown(rate=1, per=1, type=commands.BucketType.user)
+    async def add_accepted_user(self, ctx, member: discord.Member=None):
+        """
+        If a user accepts the bot terms of service, and has not
+        already accepted the terms of service, this will enter them
+        into the database so they won't need to accept the terms again.
+        """
+        member = ctx.message.author
+        member_id = ctx.message.author.id
 
-		try:
-			has_accepted = BotResources().checkAccepted(memberID, False)
+        try:
+            has_accepted = BotResources().check_accepted(member_id)
 
-			if has_accepted == True:
-				return await self.bot.say("You've already accepted my terms of service.")
-			else:
-				try:
-					query = """INSERT INTO accepted_users (discord_id) VALUES (?)"""
-					DatabaseHandler().insert_into_database(query, (str(memberID), ))
-					return await self.bot.say("{0.mention}: thanks for accepting. You may now use commands.".format(member))
-				except Exception as e:
-					await ErrorLogging().log_error(traceback.format_exc(), 'BotCommands: add_accepted_user')
-		except Exception as e:
-			await ErrorLogging().log_error(traceback.format_exc(), 'BotCommands: add_accepted_user')
+            if has_accepted:
+                return await self.bot.say("You've already accepted my terms of service.")
+            else:
+                try:
+                    query = """INSERT INTO accepted_users (discord_id) VALUES (?)"""
+                    DatabaseHandler().insert_into_database(query, (str(member_id), ))
+                    return await self.bot.say("{0.mention}: thanks for accepting. You may now \
+                                                use commands.".format(member))
+                except Exception:
+                    await ErrorLogging().log_error(
+                        traceback.format_exc(),
+                        'BotCommands: add_accepted_user'
+                    )
+        except Exception:
+            await ErrorLogging().log_error(
+                traceback.format_exc(),
+                'BotCommands: add_accepted_user'
+            )
 
-	@commands.command(pass_context=True, no_pm=True, name='nick')
-	@commands.cooldown(rate=1, per=1, type=commands.BucketType.user)
-	async def change_username(self, ctx, username: str, member: discord.Member = None):
-		member = ctx.message.author
-		memberID = ctx.message.author.id
+    @commands.command(pass_context=True, no_pm=True, name='nick')
+    @commands.cooldown(rate=1, per=1, type=commands.BucketType.user)
+    async def change_username(self, ctx, username: str, member: discord.Member=None):
+        """
+        Change the bot username.
+        """
+        member = ctx.message.author
+        member_id = ctx.message.author.id
 
-		try:
-			if memberID == ctx.message.server.owner_id or int(memberID) == ConfigLoader().load_config_setting_int('BotSettings', 'owner_id'):
-				await self.bot.change_nickname(ctx.message.server.me, username)
-				return await self.bot.say("Changed my username!")
-		except Exception as e:
-			await ErrorLogging().log_error(traceback.format_exc(), 'BotCommands: change_username')
+        try:
+            if (member_id == ctx.message.server.owner_id
+                    or int(member_id) == ConfigLoader().load_config_setting_int(
+                        'BotSettings',
+                        'owner_id'
+                    )
+               ):
+                await self.bot.change_nickname(ctx.message.server.me, username)
+                return await self.bot.say("Changed my username!")
+        except Exception:
+            await ErrorLogging().log_error(traceback.format_exc(), 'BotCommands: change_username')
 
-	@commands.command(pass_context=True, no_pm=True, name="help")
-	@commands.cooldown(rate=1, per=30, type=commands.BucketType.server)
-	async def get_help(self, ctx, member: discord.Member = None):
-		member = ctx.message.author
-		memberID = ctx.message.author.id
-
-		return await self.bot.say("{0.mention}: Please see https://xnifty.github.io for now.".format(member))
+    @commands.command(pass_context=True, no_pm=True, name="help")
+    @commands.cooldown(rate=1, per=30, type=commands.BucketType.server)
+    async def get_help(self, ctx, member: discord.Member=None):
+        """
+        Return the link for the bot documentation.
+        """
+        member = ctx.message.author
+        return await self.bot.say("{0.mention}: Please see https://xnifty.github.io \
+                                    for now.".format(member))
 
 def setup(bot):
-	"""This makes it so we can actually use it."""
-	bot.add_cog(BotCommands(bot))
+    """This makes it so we can actually use it."""
+    bot.add_cog(BotCommands(bot))
