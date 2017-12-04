aca3a2b3a2e3806730c98d321f6626b03916ee45
Ryan M <ryan@snoring.ninja>
2017-12-04 07:58:47 -0500

    Check if a private message
    If this isn't a private message to the bot, then run the message_channel_valid function like normal

--- main.py
+++ main.py
@@ -92,7 +92,8 @@ async def on_message(message):
             await CLIENT.process_commands(message)
         else:
             can_use = BotResources().check_accepted(message.author.id)
-            message_channel_valid = BotResources().get_tos_channel_valid(message.server.id)
+            if not ctx.message.channel.is_private:
+                message_channel_valid = BotResources().get_tos_channel_valid(message.server.id)
             if can_use:
                 await CLIENT.process_commands(message)
             elif not can_use and message_channel_valid:
